a45194e2ff541d5006bd5a4694cbfc9c1231e1d3
Felisky <62891363+Felisky@users.noreply.github.com>
2021-04-01 16:56:54 +0200

    cataloguev3

--- app/models/product.js
+++ app/models/product.js
@@ -2,6 +2,8 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 
 export default class ProductModel extends Model {
   @attr('string') name;
+  @attr('string') comments;
+  @attr('string') image;
   @belongsTo('section') section;
   @hasMany('product', { inverse: null }) packs;
 }
