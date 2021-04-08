188b7aa6bca92a74dd7ceced3469757d0d57a7b6
Matthieudev23 <matthieu.masse@sts-sio-caen.info>
2021-04-08 13:59:57 +0200

    Product v1

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,7 +1,17 @@
 import Route from '@ember/routing/route';
-
+import { action } from '@ember/object';
 export default class ProductsRoute extends Route {
-  model(){
-    return this.store.findAll('product',{include:'packs,section'});
+  model() {
+    return this.store.findAll('product', { include: 'packs,section' });
+  }
+
+  @action remove(p) {
+    p.deleteRecord();
+  }
+  @action delete(p){
+    p.save();
+  }
+  @action cancelremove(p){
+    p.rollbackAttributes();
   }
 }
