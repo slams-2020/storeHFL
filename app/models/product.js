eb04c528aef664146749762b44b1fefd8c9dd556
Felisky <62891363+Felisky@users.noreply.github.com>
2021-05-03 11:39:01 +0200

    Catalogue + panier

--- app/models/product.js
+++ app/models/product.js
@@ -3,6 +3,7 @@ import Model, { attr, belongsTo, hasMany } from '@ember-data/model';
 export default class ProductModel extends Model {
   @attr('string') name;
   @attr('string') comments;
+  @attr('string') stock;
   @attr('string') price;
   @attr('string') image;
   @belongsTo('section') section;
