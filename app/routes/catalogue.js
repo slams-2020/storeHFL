585df7d71b1563daf7a945ab863c31fa7bf85ed3
Felisky <62891363+Felisky@users.noreply.github.com>
2021-05-04 13:49:25 +0200

    Catalogue update

--- app/routes/catalogue.js
+++ app/routes/catalogue.js
@@ -1,7 +1,19 @@
 import Route from '@ember/routing/route';
+import { action, set } from '@ember/object';
+import RSVP from 'rsvp';
 
 export default class CatalogueRoute extends Route {
-    model(){
-    return this.store.findAll('product',{include:'packs,section'});
+  model() {
+    return RSVP.hash({
+      hide: true,
+      products: this.store.findAll('product', { include: 'packs,section' }),
+    });
+  }
+
+  @action hide(model) {
+    set(model, 'hide', false);
+    setTimeout(() => {
+      set(model, 'hide', true);
+    }, 2000);
   }
 }
